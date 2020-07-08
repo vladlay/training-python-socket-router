8016e21bba0081554ff989a18d8d17cc849f67ea
vladlay <wlawallay@gmail.com>
2020-07-08 18:40:24 +0300

    simple example

---
+++ main.py
@@ -0,0 +1,59 @@
+import socket
+from views import *
+
+URLS = {
+    '/': index,
+    '/blog': blog
+}
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return (method, url)
+
+def generatr_headers(method, url):
+    if not method == 'GET':
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+
+    if not url in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>404</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Method not allowed</p>'
+    return URLS[url]()
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generatr_headers(method, url)
+    print('headers: ', headers)
+    body = generate_content(code, url)
+
+    return (headers + body).encode()
+
+def run():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 5000))
+    server_socket.listen()
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        request = client_socket.recv(1024)
+        print('req: ', request)
+        print()
+        print('addr: ', addr)
+
+        responce = generate_response(request.decode('utf-8'))
+
+        client_socket.sendall(responce)
+        client_socket.close()
+
+if __name__ == "__main__":
+    run()
---
+++ views.py
@@ -0,0 +1,7 @@
+def index():
+    with open('templates/index.html') as template:
+        return template.read()
+
+def blog():
+    with open('templates/blog.html') as template:
+        return template.read()
